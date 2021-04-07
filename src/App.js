d695ae87a37a19da1ec6670acdf6d8d1f7b7b1c4
hkseo98 <69430175+hkseo98@users.noreply.github.com>
2021-04-07 13:09:40 +0900

    using @material-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import './App.css';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table'
+import Paper from '@material-ui/core/Paper'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    overflowX: "auto",
+    marginTop: 3,
+  },
+  table: {
+    minWidth: 1080,
+  },
+  font: {
+    fontSize: 20,
+    fontWeight: 700,
+  }
+});
 
 const customers = [
   {
@@ -28,10 +53,25 @@ const customers = [
 ]
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="gray-background">
-      {customers.map(item => <Customer key={item.id} data={item}></Customer>)}
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell className={classes.font}>번호</TableCell>
+            <TableCell className={classes.font}>이미지</TableCell>
+            <TableCell className={classes.font}>이름</TableCell>
+            <TableCell className={classes.font}>생년월일</TableCell>
+            <TableCell className={classes.font}>성별</TableCell>
+            <TableCell className={classes.font}>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map(item => <Customer key={item.id} data={item}></Customer>)}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,28 +1,34 @@
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 
 export default function Customer({data}) {
     return(
-        <div>
-            <CustomerProfile data={data}></CustomerProfile>
-            <CustomerInfo data={data}></CustomerInfo>
-        </div>
+        <TableRow>
+            <TableCell>{data.id}</TableCell>
+            <TableCell><img src={data.image} alt="profile"></img></TableCell>
+            <TableCell>{data.name}</TableCell>
+            <TableCell>{data.birth}</TableCell>
+            <TableCell>{data.sex}</TableCell>
+            <TableCell>{data.job}</TableCell>
+        </TableRow>
     )
 }
 
-function CustomerProfile({data}) {
-    return(
-        <div>
-            <img src={data.im} alt="profile"/>
-            <h2>{data.name}({data.id})</h2>
-        </div>
-    )
-}
+// function CustomerProfile({data}) {
+//     return(
+//         <div>
+//             <img src={data.image} alt="profile"/>
+//             <h2>{data.name}({data.id})</h2>
+//         </div>
+//     )
+// }
 
-function CustomerInfo({data}) {
-    return(
-        <div>
-            <p>{data.birth}</p>
-            <p>{data.gender}</p>
-            <p>{data.job}</p>
-        </div>
-    )
-}
+// function CustomerInfo({data}) {
+//     return(
+//         <div>
+//             <p>{data.birth}</p>
+//             <p>{data.gender}</p>
+//             <p>{data.job}</p>
+//         </div>
+//     )
+// }
